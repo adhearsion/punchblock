1ca27b052f02fdd3921152a9d399a874ce123f8d
Ben Klang <bklang@mojolingo.com>
2011-05-21 16:35:29 -0400

    Ensure all headers have lowercase symbols for names

--- lib/punchblock/call.rb
+++ lib/punchblock/call.rb
@@ -4,12 +4,20 @@ module Punchblock
   # This class represents an active Ozone call
   #
   class Call
-    attr_accessor :id
+    attr_accessor :id, :to, :headers
 
     def initialize(id, to, headers)
       @id = id
       @to = to
-      @headers = headers
+      # Ensure all our headers have lowercase names and convert to symbols
+      @headers = headers.inject({}) {|headers,pair|
+        headers[pair.shift.downcase.to_sym] = pair.shift
+        headers
+      }
+    end
+
+    def to_s
+      "#<Punchblock::Call:#{@id}>"
     end
   end
 end

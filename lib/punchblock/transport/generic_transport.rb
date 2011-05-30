ee56ac33bae05a4d0732fa4e744423f6b3c0775f
Ben Klang <bklang@mojolingo.com>
2011-05-30 14:26:17 -0400

    Allow the transport to parse the protocol as a string and convert to a class

--- lib/punchblock/transport/generic_transport.rb
+++ lib/punchblock/transport/generic_transport.rb
@@ -4,7 +4,11 @@ module Punchblock
       def initialize(protocol, options = {})
         @event_queue = Queue.new
         @logger = options.delete(:transport_logger) if options[:transport_logger]
-        @protocol = protocol
+        @protocol = if protocol.is_a? Class
+          protocol
+        else
+          Punchblock::Protocol.const_get(protocol.to_s.capitalize)
+        end
       end
     end
   end

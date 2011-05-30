50ea5d9e8f6e42d427872e513b28ac50db4405d8
Ben Klang <bklang@mojolingo.com>
2011-05-30 14:27:23 -0400

    Add a pile more XMPP Transport specs

--- Gemfile
+++ Gemfile
@@ -4,3 +4,7 @@ gemspec
 # Temporarily needed until Blather 0.4.17 or later is released.
 gem 'blather', :git => 'git://github.com/sprsquish/blather.git'
 
+group :development do
+  gem 'flexmock'
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,10 @@
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH.unshift File.dirname(__FILE__)
 require 'punchblock'
+require 'flexmock'
 
 RSpec.configure do |config|
-  #config.mock_with :flexmock
+  config.mock_with :flexmock
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 end

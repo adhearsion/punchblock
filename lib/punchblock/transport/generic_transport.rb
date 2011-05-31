3f2c95b1801c63907db52d206065637d51f8352c
Ben Klang <bklang@mojolingo.com>
2011-05-31 17:04:38 -0400

    We want a Module, not a Class here

--- lib/punchblock/transport/generic_transport.rb
+++ lib/punchblock/transport/generic_transport.rb
@@ -4,7 +4,7 @@ module Punchblock
       def initialize(protocol, options = {})
         @event_queue = Queue.new
         @logger = options.delete(:transport_logger) if options[:transport_logger]
-        @protocol = if protocol.is_a? Class
+        @protocol = if protocol.is_a? Module
           protocol
         else
           Punchblock::Protocol.const_get(protocol.to_s.capitalize)

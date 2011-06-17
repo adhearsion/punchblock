0d656ade3f6377d8f8f9830ffdb27985cd397008
Ben Langfeld <ben@langfeld.me>
2011-06-17 15:37:12 +0100

    Commands should know their execution status, based on events received

--- lib/punchblock/protocol/ozone/command.rb
+++ lib/punchblock/protocol/ozone/command.rb
@@ -1,3 +1,5 @@
+require 'state_machine'
+
 module Punchblock
   module Protocol
     class Ozone
@@ -26,6 +28,25 @@ module Punchblock
           def add_event(event)
             event.original_command = self
             @events << event
+            transition_state! event
+          end
+
+          def transition_state!(event)
+            complete! if event.is_a? Ozone::Event::Complete
+          end
+
+          state_machine :state, :initial => :new do
+            event :request do
+              transition :new => :requested
+            end
+
+            event :execute do
+              transition :requested => :executing
+            end
+
+            event :complete do
+              transition :executing => :complete
+            end
           end
         end # CommandNode
       end # Command

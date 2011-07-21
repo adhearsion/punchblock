5b4733d7025d7f898b61c76f7c2ff52472adbb2d
Ben Langfeld <ben@langfeld.me>
2011-07-21 12:11:00 +0100

    Revert "Swallow xmpp ping errors"
    This reverts commit 89144d675a49c37beaa550acf92458a2247e96ca.

--- lib/punchblock/protocol/ozone/connection.rb
+++ lib/punchblock/protocol/ozone/connection.rb
@@ -221,7 +221,13 @@ module Punchblock
         end
 
         def ping_ozone
-          client.write_with_handler Blather::Stanza::Iq::Ping.new(:set, @ozone_domain) { |response| nil }
+          client.write_with_handler Blather::Stanza::Iq::Ping.new(:set, @ozone_domain) do |response|
+            begin
+              handle_error response if response.is_a? Blather::BlatherError
+            rescue ProtocolError => e
+              raise e unless e.name == :feature_not_implemented
+            end
+          end
         end
 
         def create_iq(jid = nil)

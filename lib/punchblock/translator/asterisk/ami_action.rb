567435e8abef0d0e4909d904d6ccd9d0f89622d3
Ben Langfeld <ben@langfeld.me>
2011-11-08 13:09:17 +0000

    Causal actions should trigger events from the component, and set the complete event properly on the component node, including attributes from the terminating event

--- lib/punchblock/translator/asterisk/ami_action.rb
+++ lib/punchblock/translator/asterisk/ami_action.rb
@@ -23,6 +23,7 @@ module Punchblock
             headers[key.to_s.capitalize] = value
           end
           RubyAMI::Action.new @component_node.name, headers do |response|
+            send_events
             send_complete_event
           end
         end
@@ -37,12 +38,36 @@ module Punchblock
 
         def complete_event
           headers = @action.response.headers
+          headers.merge! @extra_complete_attributes if @extra_complete_attributes
           headers.delete 'ActionID'
-          Punchblock::Component::Asterisk::AMI::Action::Complete::Success.new :message => headers.delete('Message'), :attributes => headers
+          Punchblock::Event::Complete.new.tap do |c|
+            c.reason = Punchblock::Component::Asterisk::AMI::Action::Complete::Success.new :message => headers.delete('Message'), :attributes => headers
+          end
+        end
+
+        def send_events
+          return unless @action.has_causal_events?
+          @action.events.each do |e|
+            if e.name.downcase == @action.causal_event_terminator_name
+              @extra_complete_attributes = e.headers
+            else
+              send_event pb_event_from_ami_event(e)
+            end
+          end
+        end
+
+        def pb_event_from_ami_event(ami_event)
+          headers = ami_event.headers
+          headers.delete 'ActionID'
+          Event::Asterisk::AMI::Event.new :name => ami_event.name, :attributes => headers
+        end
+
+        def send_event(event)
+          @component_node.add_event event
         end
 
         def send_complete_event
-          @component_node.complete_event.resource = complete_event
+          send_event complete_event
         end
       end
     end
--- spec/punchblock/translator/asterisk/ami_action_spec.rb
+++ spec/punchblock/translator/asterisk/ami_action_spec.rb
@@ -16,10 +16,6 @@ module Punchblock
           RubyAMI::Action.new 'ExtensionStatus', 'Context' => 'default', 'Exten' => 'idonno'
         end
 
-        let :expected_complete_event do
-          Punchblock::Component::Asterisk::AMI::Action::Complete::Success.new :message => 'Channel status will follow', :attributes => {:exten => "idonno", :context => "default", :hint => "", :status => "-1"}
-        end
-
         context 'initial execution' do
           let(:component_id) { UUIDTools::UUID.random_create }
 
@@ -37,7 +33,12 @@ module Punchblock
         end
 
         context 'when the AMI action completes' do
-          let(:response) do
+          before do
+            command.request!
+            command.execute!
+          end
+
+          let :response do
             RubyAMI::Response.new.tap do |r|
               r['ActionID'] = "552a9d9f-46d7-45d8-a257-06fe95f48d99"
               r['Message']  = 'Channel status will follow'
@@ -48,10 +49,82 @@ module Punchblock
             end
           end
 
-          it 'should send a complete event to the component node' do
-            subject.action.response = response
+          let :expected_complete_reason do
+            Punchblock::Component::Asterisk::AMI::Action::Complete::Success.new :message => 'Channel status will follow', :attributes => {:exten => "idonno", :context => "default", :hint => "", :status => "-1"}
+          end
 
-            command.complete_event.resource(0.5).should == expected_complete_event
+          context 'for a non-causal action' do
+            it 'should send a complete event to the component node' do
+              subject.action.response = response
+
+              command.should be_complete
+
+              command.complete_event.resource(0.5).reason.should == expected_complete_reason
+            end
+          end
+
+          context 'for a causal action' do
+            let :command do
+              Punchblock::Component::Asterisk::AMI::Action.new :name => 'CoreShowChannels'
+            end
+
+            let :expected_action do
+              RubyAMI::Action.new 'CoreShowChannels'
+            end
+
+            let :event do
+              RubyAMI::Event.new('CoreShowChannel').tap do |e|
+                e['ActionID']         = "552a9d9f-46d7-45d8-a257-06fe95f48d99"
+                e['Channel']          = 'SIP/127.0.0.1-00000013'
+                e['UniqueID']         = '1287686437.19'
+                e['Context']          = 'adhearsion'
+                e['Extension']        = '23432'
+                e['Priority']         = '2'
+                e['ChannelState']     = '6'
+                e['ChannelStateDesc'] = 'Up'
+              end
+            end
+
+            let :terminating_event do
+              RubyAMI::Event.new('CoreShowChannelsComplete').tap do |e|
+                e['EventList'] = 'Complete'
+                e['ListItems'] = '3'
+                e['ActionID'] = 'umtLtvSg-RN5n-GEay-Z786-YdiaSLNXkcYN'
+              end
+            end
+
+            let :event_node do
+              Punchblock::Event::Asterisk::AMI::Event.new :name => 'CoreShowChannel', :attributes => {
+                :channel          => 'SIP/127.0.0.1-00000013',
+                :uniqueid         => '1287686437.19',
+                :context          => 'adhearsion',
+                :extension        => '23432',
+                :priority         => '2',
+                :channelstate     => '6',
+                :channelstatedesc => 'Up'
+              }
+            end
+
+            let :expected_complete_reason do
+              Punchblock::Component::Asterisk::AMI::Action::Complete::Success.new :message => 'Channel status will follow', :attributes => {:exten => "idonno", :context => "default", :hint => "", :status => "-1", :eventlist => 'Complete', :listitems => '3'}
+            end
+
+            it 'should send events to the component node' do
+              command.register_event_handler Punchblock::Event::Asterisk::AMI::Event do |event|
+                @event = event
+              end
+              subject.action << event
+              subject.action << response
+              subject.action << terminating_event
+              @event.should == event_node
+            end
+
+            it 'should send a complete event to the component node' do
+              subject.action << response
+              subject.action << terminating_event
+
+              command.complete_event.resource(0.5).reason.should == expected_complete_reason
+            end
           end
         end
       end

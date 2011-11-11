0d377e9a86eb411ed82fe4c25363d7539e9d02fe
Ben Langfeld <ben@langfeld.me>
2011-11-11 09:47:21 +0000

    Use a single logger for all of Punchblock, accessible via Object#pb_logger (so it can be overridden per class or per instance)

--- lib/punchblock/connection/asterisk.rb
+++ lib/punchblock/connection/asterisk.rb
@@ -7,8 +7,7 @@ module Punchblock
       attr_accessor :event_handler
 
       def initialize(options = {})
-        options[:logger] = options[:wire_logger]
-        @ami_client = RubyAMI::Client.new options.merge(:event_handler => lambda { |event| translator.handle_ami_event! event })
+        @ami_client = RubyAMI::Client.new options.merge(:event_handler => lambda { |event| translator.handle_ami_event! event }, :logger => pb_logger)
         @translator = Translator::Asterisk.new @ami_client, self
         super()
       end

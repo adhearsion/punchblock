bf629d889512017836c156b937bc8714cc0ba0dd
Ben Langfeld <ben@langfeld.me>
2011-11-23 13:34:29 +0000

    [FEATURE] Asterisk media output should send a complete event when all elements have been executed

--- lib/punchblock/translator/asterisk/component/asterisk/output.rb
+++ lib/punchblock/translator/asterisk/component/asterisk/output.rb
@@ -12,37 +12,58 @@ module Punchblock
             end
 
             def execute
-              @component_node.ssml.children.each do |node|
+              set_node_response Ref.new :id => id
+
+              @component_node.ssml or return
+              @execution_elements = @component_node.ssml.children.map do |node|
                 case node
                 when RubySpeech::SSML::Audio
-                  @call.send_agi_action! 'STREAM FILE', node.src, '"'
+                  lambda { current_actor.play_audio! node.src }
                 end
+              end
+
+              @pending_actions = @execution_elements.count
+
+              @execution_elements.each &:call
+            end
+
+            def process_playback_completion
+              @pending_actions -= 1
+              pb_logger.debug "Received action completion. Now waiting on #{@pending_actions} actions."
+              if @pending_actions < 1
+                pb_logger.debug "Sending complete event"
+                send_event complete_event(success_reason)
               end
             end
 
-            # private
+            def play_audio(path)
+              @call.send_agi_action! 'STREAM FILE', path, '"' do |complete_event|
+                current_actor.process_playback_completion
+              end
+            end
 
-            # def set_node_response(value)
-            #   pb_logger.debug "Setting response on component node to #{value}"
-            #   @component_node.response = value
-            # end
+            private
 
-            # def success_reason(event)
-            #   code, result, data = parse_agi_result event['Result']
-            #   Punchblock::Component::Asterisk::AGI::Command::Complete::Success.new :code => code, :result => result, :data => data
-            # end
+            def set_node_response(value)
+              pb_logger.debug "Setting response on component node to #{value}"
+              @component_node.response = value
+            end
 
-            # def complete_event(reason)
-            #   Punchblock::Event::Complete.new.tap do |c|
-            #     c.reason = reason
-            #   end
-            # end
+            def success_reason
+              Punchblock::Component::Output::Complete::Success.new
+            end
 
-            # def send_event(event)
-            #   event.component_id = id
-            #   pb_logger.debug "Sending event #{event.inspect}"
-            #   @component_node.add_event event
-            # end
+            def complete_event(reason)
+              Punchblock::Event::Complete.new.tap do |c|
+                c.reason = reason
+              end
+            end
+
+            def send_event(event)
+              event.component_id = id
+              pb_logger.debug "Sending event #{event.inspect}"
+              @component_node.add_event event
+            end
           end
         end
       end
--- spec/punchblock/translator/asterisk/component/asterisk/output_spec.rb
+++ spec/punchblock/translator/asterisk/component/asterisk/output_spec.rb
@@ -6,8 +6,9 @@ module Punchblock
       module Component
         module Asterisk
           describe Output do
-            let(:channel)       { 'SIP/foo' }
-            let(:mock_call)     { mock 'Call' }#, :channel => channel }
+            let(:channel)         { 'SIP/foo' }
+            let(:mock_call)       { mock 'Call' }#, :channel => channel }
+            let(:command_options) { nil }
 
             let :command do
               Punchblock::Component::Output.new command_options
@@ -15,40 +16,67 @@ module Punchblock
 
             subject { Output.new command, mock_call }
 
-            context 'with a media engine of :asterisk' do
-              context 'with a single audio SSML node' do
-                let(:audio_filename) { 'http://foo.com/bar.mp3' }
-                let :command_options do
-                  filename = audio_filename
-                  {
-                    :ssml => RubySpeech::SSML.draw { audio :src => filename }
-                  }
-                end
+            describe '#execute' do
+              before { command.request! }
 
-                it 'should playback the audio file using STREAM FILE' do
-                  mock_call.expects(:send_agi_action!).once.with 'STREAM FILE', audio_filename, '"'
-                  subject.execute
-                end
+              it 'should send a ref response immediately' do
+                subject.execute
+                command.component_id.should == subject.id
               end
 
-              context 'with multiple audio SSML nodes' do
-                let(:audio_filename1) { 'http://foo.com/bar.mp3' }
-                let(:audio_filename2) { 'http://foo.com/baz.mp3' }
-                let :command_options do
-                  filename1 = audio_filename1
-                  filename2 = audio_filename2
-                  {
-                    :ssml => RubySpeech::SSML.draw do
-                      audio :src => filename1
-                      audio :src => filename2
+              context 'with a media engine of :asterisk' do
+                context 'with a single audio SSML node' do
+                  let(:audio_filename) { 'http://foo.com/bar.mp3' }
+                  let :command_options do
+                    filename = audio_filename
+                    {
+                      :ssml => RubySpeech::SSML.draw { audio :src => filename }
+                    }
+                  end
+
+                  it 'should playback the audio file using STREAM FILE' do
+                    mock_call.expects(:send_agi_action!).once.with 'STREAM FILE', audio_filename, '"'
+                    subject.execute
+                  end
+
+                  it 'should send a complete event when the file finishes playback' do
+                    def mock_call.send_agi_action!(*args, &block)
+                      block.call Punchblock::Component::Asterisk::AGI::Command::Complete::Success.new(:code => 200, :result => 1)
                     end
-                  }
+                    subject.execute
+                    command.complete_event(0.1).reason.should be_a Punchblock::Component::Output::Complete::Success
+                  end
                 end
 
-                it 'should playback each audio file using STREAM FILE' do
-                  mock_call.expects(:send_agi_action!).once.with 'STREAM FILE', audio_filename1, '"'
-                  mock_call.expects(:send_agi_action!).once.with 'STREAM FILE', audio_filename2, '"'
-                  subject.execute
+                context 'with multiple audio SSML nodes' do
+                  let(:audio_filename1) { 'http://foo.com/bar.mp3' }
+                  let(:audio_filename2) { 'http://foo.com/baz.mp3' }
+                  let :command_options do
+                    filename1 = audio_filename1
+                    filename2 = audio_filename2
+                    {
+                      :ssml => RubySpeech::SSML.draw do
+                        audio :src => filename1
+                        audio :src => filename2
+                      end
+                    }
+                  end
+
+                  it 'should playback each audio file using STREAM FILE' do
+                    mock_call.expects(:send_agi_action!).once.with 'STREAM FILE', audio_filename1, '"'
+                    mock_call.expects(:send_agi_action!).once.with 'STREAM FILE', audio_filename2, '"'
+                    subject.execute
+                  end
+
+                  it 'should send a complete event after the final file has finished playback' do
+                    def mock_call.send_agi_action!(*args, &block)
+                      block.call Punchblock::Component::Asterisk::AGI::Command::Complete::Success.new(:code => 200, :result => 1)
+                    end
+                    command.expects(:add_event).once.with do |e|
+                      e.reason.should be_a Punchblock::Component::Output::Complete::Success
+                    end
+                    subject.execute
+                  end
                 end
               end
             end

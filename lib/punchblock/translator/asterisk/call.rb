dd76fec352aa3f952ec4c5eacfcb1e939e903d4c
Ben Langfeld <ben@langfeld.me>
2012-03-11 15:23:53 +0000

    [CS] Don't log forwarding events from call to component

--- lib/punchblock/translator/asterisk/call.rb
+++ lib/punchblock/translator/asterisk/call.rb
@@ -111,7 +111,6 @@ module Punchblock
           when 'AsyncAGI'
             pb_logger.debug "Received an AsyncAGI event. Looking for matching AGICommand component."
             if component = component_with_id(ami_event['CommandID'])
-              pb_logger.debug "Found component #{component.id} for event. Forwarding event..."
               component.handle_ami_event! ami_event
             else
               pb_logger.warn "Could not find component for AMI event: #{ami_event.inspect}"

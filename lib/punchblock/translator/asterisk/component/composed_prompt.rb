a5586e0711b473f17fc1746c95524f1607df2f08
Ben Langfeld <ben@langfeld.me>
2013-05-10 12:05:06 -0300

    [CS] Shouldn't be swallowing these failures

--- lib/punchblock/translator/asterisk/component/composed_prompt.rb
+++ lib/punchblock/translator/asterisk/component/composed_prompt.rb
@@ -28,8 +28,6 @@ module Punchblock
 
             register_dtmf_event_handler
             start_timers
-          rescue => e
-            pb_logger.error e
           end
 
           def process_dtmf(digit)

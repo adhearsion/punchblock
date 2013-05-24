7e91fce1f9b7dba848c64dc52eace9bf755367b5
Ben Langfeld <ben@langfeld.me>
2013-05-24 19:03:16 -0300

    [BUGFIX] Do not keep Asterisk Call actors around after hangup

--- lib/punchblock/translator/asterisk/call.rb
+++ lib/punchblock/translator/asterisk/call.rb
@@ -295,7 +295,7 @@ module Punchblock
         def send_end_event(reason)
           send_pb_event Event::End.new(:reason => reason)
           translator.deregister_call current_actor
-          after(5) { shutdown }
+          terminate
         end
 
         def execute_component(type, command, options = {})

90ba97f3b1b763d55282dae74776f99b9ca8ab9d
Ben Langfeld <ben@langfeld.me>
2014-03-04 12:27:10 -0300

    [BUGFIX] Rayo events should not include their timestamp in comparison.
    This is not useful in applications, and makes testing more difficult.
    
    Conflicts:
    	CHANGELOG.md

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,9 +28,6 @@ RSpec.configure do |config|
   config.before do
     @uuid = SecureRandom.uuid
     Punchblock.stub new_request_id: @uuid
-
-    @current_datetime = DateTime.now
-    DateTime.stub now: @current_datetime
   end
 
   config.after :each do
